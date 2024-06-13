e0bcb60fae79c20bb2f7a17829bfa5c6f9a359dd
Hikari06 <106168976+Hikari06@users.noreply.github.com>
2024-06-13 13:30:25 -0300

    Update main.jsx

--- KumaMovie 1/KumaMovie/KumaMovie/src/main.jsx
+++ KumaMovie 1/KumaMovie/KumaMovie/src/main.jsx
@@ -8,7 +8,7 @@ import Filmes from './pages/Filmes.jsx'
 import Sobre from './pages/Sobre.jsx'
 import Contato from './pages/Contato.jsx'
 import Home from './pages/Home.jsx'
-import Home from './pages/PaginasFilmes.jsx'
+import PaginasFilmes from './pages/PaginasFilmes.jsx'
 
 const router = createBrowserRouter(
   [
@@ -18,7 +18,7 @@ const router = createBrowserRouter(
       children: [
         {index: true, element: <Home/> },
         {path: "filmes", element: <Filmes/>},
-        {path: 'filmes/:id', element: <PaginaFilmes/>},
+        {path: 'filmes/:id', element: <PaginasFilmes/>},
         {path: "sobre", element: <Sobre/>},
         {path: "contato", element: <Contato/>},
         {path: "*", element: <PageNotFound/>}
