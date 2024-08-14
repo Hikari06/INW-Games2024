59dc5e63bc452c83c92689f4b3a08f09579d7b12
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 11:56:44 -0300

    Update 14/08/24

--- KumaMovie 1/KumaMovie/KumaMovie/src/componentes/Header/Header.jsx
+++ KumaMovie 1/KumaMovie/KumaMovie/src/componentes/Header/Header.jsx
@@ -9,13 +9,6 @@ function Header() {
             <Navbar/>
         </header>
         
-        <input
-        type="search"
-        id="default-search"
-        className=" w-xl  h-15 p-4 ps-10 mt-5 mb-5 mr-5 ml-5 text-sm text-primary-claro border border-primary-ocre rounded-full bg-white "
-        placeholder="Search Mockups, Logos..."
-        required
-        />
         </div>
         </>
         );
--- KumaMovie 1/KumaMovie/KumaMovie/src/componentes/Navbar/Navbar.jsx
+++ KumaMovie 1/KumaMovie/KumaMovie/src/componentes/Navbar/Navbar.jsx
@@ -3,14 +3,21 @@ import {Link} from 'react-router-dom'
 function Navbar() {
     return ( 
         <nav>
-            <ul className='flex gap-20'>
-                <Link to ="/"><img  src="../../KumaMovieLogo.png" className='h-10 flex-row-start mr-50' alt="" /></Link>
-                <li><Link to ="/"> Home</Link></li>
-                <li><Link to ="filmes"> Filmes </Link></li>
-                <li><Link to ="blog"> Blog </Link></li>
-                <li><Link to ="contato"> Contato </Link></li>
-                <li><Link to="login">Login</Link></li>
-                <Link to ="login"><img  src="../../Usuario.png" className='h-10 flex-row-end ml-50' alt="" /></Link>
+            <ul className='flex grid-cols-1 gap-20'>
+                <Link to ="/"><img  src="../../KumaMovieLogo.png" className='w-5/12 m-auto' alt="" /></Link>
+                <input
+                type="search"
+                id="default-search"
+                className=" w-xl  h-15 p-4 ps-10 mt-5 mb-5 mr-5 ml-5 text-sm text-primary-claro border border-primary-ocre rounded-full bg-white "
+                placeholder="Search Mockups, Logos..."
+                required
+                />
+                <li className='mt-5 mb-5 '><Link to ="/"> Home</Link></li>
+                <li className='mt-5 mb-5 '><Link to ="filmes"> Filmes </Link></li>
+                <li className='mt-5 mb-5 '><Link to ="blog"> Blog </Link></li>
+                <li className='mt-5 mb-5 '><Link to ="contato"> Contato </Link></li>
+                <li className='mt-5 mb-5 '><Link to="login">Login</Link></li>
+                <Link to ="login"><img  src="../../Usuario.png" className='h-10 flex-end ml-50' alt="" /></Link>
             </ul>
         </nav>
         );
