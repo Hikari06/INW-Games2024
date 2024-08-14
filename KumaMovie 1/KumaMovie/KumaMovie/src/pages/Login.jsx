c2d2757eeb4c523645cf949e9ad798a7721c4f65
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 12:11:43 -0300

    Update 14/08/24

--- KumaMovie 1/KumaMovie/KumaMovie/src/pages/Login.jsx
+++ KumaMovie 1/KumaMovie/KumaMovie/src/pages/Login.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 function Login() {
     const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleSubmit = (event) => {
       event.preventDefault();
@@ -11,17 +13,36 @@ function Login() {
  
   
     return (
-    <main className='bg-primary-claro'>
-        <label>Enter your name:
+    <main className='bg-primary-claro h-11'>
+        <label className='mt-5'>Enter your name:
         <input 
           type="text" 
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
       </label>
+
+      <label className='mt-5'>Enter your email:
+        <input 
+          type="text" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+
+      <label className='mt-5'>Enter your password:
+        <input 
+          type="text" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
       <input type="submit" />
     </main>
    );
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Login />);
 
 export default Login ;
