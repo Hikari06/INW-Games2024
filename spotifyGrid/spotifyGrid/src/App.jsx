0abbe3e3be044d8397f9715af8dac1217e708e05
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 12:18:11 -0300

    spotify grid

---
+++ spotifyGrid/spotifyGrid/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="header bg-gray-800 h-24 w-50">
+      <div className="side ">
+      <div className="bg-slate-700 h-30 w-screen"></div>
+        <div className="grid grid-cols-4 gap-5 bg-slate-500 h-screen w-full">
+          <div className="bg-red-400 h-10 w-10 rounded-lg mx-10"></div>
+      </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
